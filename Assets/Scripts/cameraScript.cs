111ad63ef2e8ddb4125934469622d8561c926c32
Anthony Yuen <anthonyyuen1992@hotmail.com>
2016-01-30 17:04:43 -0500

    added basic player movement with camera tracking

---
+++ Assets/Scripts/cameraScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraScript : MonoBehaviour {
+
+	public Transform target;
+	public float cameraDistance = -10f;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find("player1").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.position + new Vector3(0, 0, cameraDistance);
+	}
+}
---
+++ Assets/Scripts/movePlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePlayer : MonoBehaviour {
+
+	public float playerSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float moveX = Input.GetAxisRaw("Horizontal");
+		float moveY = Input.GetAxisRaw("Vertical");
+		transform.Translate(Vector2.right * moveX * playerSpeed * Time.deltaTime);
+		transform.Translate(Vector2.up * moveY * playerSpeed * Time.deltaTime);
+	}
+}
