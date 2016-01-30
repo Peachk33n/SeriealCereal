4c4c0a66341a0a9c916a90da0c47ac7054f8625c
Peachk33n <peachkeen@rocketmail.com>
2016-01-30 13:16:15 -0500

    playing with gyro

--- Assets/Scripts/Eating Mini Game/EatingGameManager.cs
+++ Assets/Scripts/Eating Mini Game/EatingGameManager.cs
@@ -5,8 +5,7 @@ public class EatingGameManager : MonoBehaviour {
 	private static EatingGameManager instance = null;
 	public static EatingGameManager Instance { get { return instance; } }
 
-	private Gyroscope mGyro = new Gyroscope();
-
+	private bool gyroInitialized = false;
 
 	void Awake(){
 		if(instance != null && instance != this)
@@ -22,6 +21,45 @@ public class EatingGameManager : MonoBehaviour {
 	}
 
 	void Start(){
-		mGyro.
+		/*mGyro.updateInterval = 1.0f;
+		Debug.Log("enabled: " + mGyro.enabled);
+		Debug.Log("updateInterval: " + mGyro.updateInterval);*/
+	}
+
+	void Update(){
+		/*Debug.Log("attitude: " + mGyro.attitude);
+		Debug.Log("gravity: " + mGyro.gravity);
+		Debug.Log("rotationRate: " + mGyro.rotationRate);
+		Debug.Log("rotationRateUnbiased: " + mGyro.rotationRateUnbiased);
+		Debug.Log("userAcceleration: " + mGyro.userAcceleration);*/
+
+		Debug.Log("the Quat: " + Get());
+	}
+
+	public bool HasGyroscope{
+		get{
+			return SystemInfo.supportsGyroscope;
+		}
+	}
+
+	public Quaternion Get(){
+		if(!gyroInitialized){
+			InitGyro();
+		}
+		return HasGyroscope
+			? ReadGyroScopeRotation()
+				: Quaternion.identity;
+	}
+
+	private void InitGyro(){
+		if(HasGyroscope){
+			Input.gyro.enabled = true; //enable the gyroscope
+			Input.gyro.updateInterval = 0.0167f; //the highest update value (60 Hz)
+		}
+		gyroInitialized = true;
+	}
+
+	private Quaternion ReadGyroScopeRotation(){
+		return new Quaternion(0.5f, 0.5f, -0.5f, 0.05f) * Input.gyro.attitude * new Quaternion(0, 0, 1, 0);
 	}
 }
