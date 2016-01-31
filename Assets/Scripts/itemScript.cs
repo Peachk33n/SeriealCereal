6c79e2311a6d527914dbfb632f49d46e9648a782
Anthony Yuen <anthonyyuen1992@hotmail.com>
2016-01-30 20:00:52 -0500

    player can now use mouse input for movement and item selection

---
+++ Assets/Scripts/itemScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class itemScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void ActedUpon() {
+		Debug.Log("I was acted upon by the player!");
+		// Do stuff
+	}
+}
--- Assets/Scripts/movePlayer.cs
+++ Assets/Scripts/movePlayer.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class movePlayer : MonoBehaviour {
 
 	public float playerSpeed = 1f;
+	private float minItemDistance = 1f;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 
 	// Update is called once per frame
@@ -16,5 +17,28 @@ public class movePlayer : MonoBehaviour {
 		float moveY = Input.GetAxisRaw("Vertical");
 		transform.Translate(Vector2.right * moveX * playerSpeed * Time.deltaTime);
 		transform.Translate(Vector2.up * moveY * playerSpeed * Time.deltaTime);
+		if (Input.GetMouseButton(0)) {
+			// item selection using "mouse"
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+			if (hit.collider != null) { // if the raycast hit something
+				var itemDistance = Vector2.Distance(transform.position, hit.collider.transform.position);
+				Debug.Log("Trying to select " + hit.collider.transform.name 
+					+ " which is " + itemDistance + " away");
+				if (itemDistance <= minItemDistance) { // if select succesful
+					Debug.Log("Acting upon: " + hit.collider.transform.name);
+					// send the selected object "ActedUpon"
+					hit.collider.gameObject.SendMessage("ActedUpon");
+				}
+			}
+			// move player towards the current mouse position when mouse is down
+			var targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			targetPos.z = transform.position.z;
+			transform.position = Vector3.MoveTowards(transform.position, targetPos, playerSpeed * Time.deltaTime);
+		}
+	}
+
+	void ActedUpon() {
+		Debug.Log("Cereal.");
 	}
 }
